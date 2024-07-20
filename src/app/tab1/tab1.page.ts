fe2ea6266bcbf2543451d2b018113254284f9e26
BenKai3 <benjaminerkan@gmail.com>
2024-07-20 15:11:57 -0700

    adding week 3 capabilities: dynamic rendering of items list, as well as adding & removing items

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,79 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "Grocery App"
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+    {
+      name: "Banana",
+      quantity: 3
+    },
+    {
+      name: "Sugar",
+      quantity: 1
+    }
+  ]
+  
+  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController) {}
 
+  async removeItem(item: Item, index: number) {
+    console.log(`removing item ${item.name} index: ${index}`)
+    const toast = await this.toastCtrl.create({
+      message: `Removing Item - ${item.name}...`,
+      duration: 3000
+    })
+    await toast.present()
+    this.items.splice(index, 1)
+  }
+
+  addItem() {
+    console.log("adding item")
+    this.showAddItemPrompt()
+  }
+
+  async showAddItemPrompt() {
+    const prompt = await this.alertCtrl.create({
+      header: 'Add Item',
+      // message: "add an item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked')
+          }
+        },
+        {
+          text: 'Add',
+          handler: item => {
+            console.log('Add clicked', item)
+            this.items.push(item)
+          }
+        }
+      ]
+    })
+    await prompt.present()
+  }
+
+}
+
+interface Item {
+  name: string;
+  quantity: number;
 }
